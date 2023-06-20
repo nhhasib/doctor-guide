ddcdfab2bf87fdc8bff657912e3e4a1a1a6a238e
nhhasib <nafiul.hasan1997@gmail.com>
2023-06-21 00:54:46 +0600

    animation

--- src/Componenets/SickKid.jsx
+++ src/Componenets/SickKid.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import img from '../assets/images/kisspng-physician-nursing-medicine-health-care-foreign-doctor-5a6b98c3c69386.png'
 import bg from '../assets/images/Rectangle 46.png'
 import medical from '../assets/images/medical.png'
+import { motion } from "framer-motion"
 
 const SickKid = () => {
     return (
         <div className='w-4/5 lg:flex mx-auto items-center mt-[1800px] lg:mt-[600px] gap-12'>
             <div className='items-center relative mb-72 lg:mb-0'>
-                <img className='' src={bg} alt="" />
+                <motion.img  initial={{ scale: 0 }}
+  animate={{ rotate: 180, scale: 1 }}
+  transition={{
+    type: "spring",
+    stiffness: 260,
+    damping: 20
+  }} className='' src={bg} alt="" />
                 <img className='w-[436px] h-[614px] absolute -top-48 lg:left-20' src={img} alt="" />
             </div>
             <div>
